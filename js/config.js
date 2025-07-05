4b39d8ee7a63c37392a03c6d963193a7a6000938
Hamdi Kandil <hamdikendil.insatgmail.com>
2025-07-06 00:39:29 +0100

    new_config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_bspuDgdOI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1lpggs8udfgg4d95tf8gqfe5b5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://main.d4a4tv0s931ng.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
